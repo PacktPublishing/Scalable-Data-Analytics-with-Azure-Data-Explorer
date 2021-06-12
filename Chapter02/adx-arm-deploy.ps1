52e13fd1668fe53975e7849b9b32999ce695f0a4
Jason <jmyerscough1@gmail.com>
2021-06-12 14:45:43 +0200

    Added PowerShell commands for deploying the ARM templates

---
+++ Chapter02/adx-arm-deploy.ps1
@@ -0,0 +1,12 @@
+
+# If you want to deploy your ARM templates from your local machine rather than Cloud Shell, then
+# you need to authenticate to Azure by using the following command.
+# Connect-AzAccount
+
+# Create a resource group. This is where we will deploy our ADX cluster to.
+# You can use Get-AzLocation to get a list of available locations. West Europe
+# is the closest AZ location to me.
+New-AzResourceGroup -Name "adx-rg" -Location "westeurope"
+
+# Deploy our ARM templates to the resource group we just created.
+New-AzResourceGroupDeployment -Name "AdxDeployment" -ResourceGroupName "adx-rg" -TemplateFile ./adx-arm.json -TemplateParameterFile ./adx-arm.params.json
