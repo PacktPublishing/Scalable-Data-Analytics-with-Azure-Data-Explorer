aa357d95c43bf8e8b9388b02ba64b7cf004ca504
Jason <jmyerscough1@gmail.com>
2021-08-07 15:11:07 +0200

    added the -Force arg to stop PowerShell prompting the user.

--- Chapter02/adx-arm-deploy.ps1
+++ Chapter02/adx-arm-deploy.ps1
@@ -6,7 +6,7 @@
 # Create a resource group. This is where we will deploy our ADX cluster to.
 # You can use Get-AzLocation to get a list of available locations. West Europe
 # is the closest AZ location to me.
-New-AzResourceGroup -Name "adx-rg" -Location "westeurope"
+New-AzResourceGroup -Name "adx-rg" -Location "westeurope" -Force
 
 # Deploy our ARM templates to the resource group we just created.
 New-AzResourceGroupDeployment -Name "AdxDeployment" -ResourceGroupName "adx-rg" -TemplateFile ./adx-arm.json -TemplateParameterFile ./adx-arm.params.json
